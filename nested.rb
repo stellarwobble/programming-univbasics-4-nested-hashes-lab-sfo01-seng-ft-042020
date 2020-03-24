f5283f4f01abcde4ce9deaa9f666b5e5614d3708
Jennifer Paquette <jennidemuir@gmail.com>
2020-03-24 03:24:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -108,7 +108,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-programmer_hash[:alan_kay][:known_for]= alans_new_info
+programmer_hash[:alan_kay][:known_for]= "GUI"
 return programmer_hash
 end
 
