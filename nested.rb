4c0d3958dedb957c83e98bfed891cd89a742efdc
Jennifer Paquette <jennidemuir@gmail.com>
2020-03-24 02:01:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,6 +86,7 @@ programmer_hash.unshift(:yukihiro_matsumoto => {
 #   :languages => ["LISP", "C"]
 # })
 end
+end
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
